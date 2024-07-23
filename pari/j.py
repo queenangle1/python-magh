9424a98002573bbeaecbff5d70d7bd11903890b2
pari@gmail.com <pari@gmail.com>
2024-07-23 16:29:08 +0545

    first commit

---
+++ pari/j.py
@@ -0,0 +1,20 @@
+#name="pari"
+#print(name)
+
+#print("hello world\n"*3)
+#print("my name is pari\n"*5)
+""" i my me mine our your yourself we was were 
+warm going same come gain grabb"""
+#print (multiline comment)
+
+a=1
+b=2
+c=a
+b=c
+a=b
+#print(a,b)
+a=4
+b=3
+a,b=b,a
+print(a,b)
+print(a+b)
